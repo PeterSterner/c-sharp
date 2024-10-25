9d0bb5692c2cd3a415f2f2f25dd1749482d0bf25
petey33 <psterner@gmail.com>
2024-10-25 15:32:37 +0200

    feat: Call ListerIListerEksempler method statically in Program.cs

--- Program.cs
+++ Program.cs
@@ -57,8 +57,8 @@ namespace Programmering
             */
 
             // ListerILister
-            Lister l = new();
-            l.ListerIListerEksempler();
+            Lister.ListerIListerEksempler();
+
 
         }
     }
